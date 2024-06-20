3ece666d962d6f4b9a71ce294ca3f8ce21000136
phantrunghieu91 <phantrunghieu1291@gmail.com>
2024-06-20 11:16:16 +0700

    first commit'

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+const Customers = () => {
+  return <h1>Customers Page</h1>;
+};
+
+export default Customers;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+const Invoices = () => {
+  return (
+    <h1>Invoices page</h1>
+  )
+}
+
+export default Invoices;
